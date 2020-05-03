4ff0334025701324c27c95daf1cfd5e431674acb
wyattm14 <wmoe14@outlook.com>
2020-05-03 22:19:49 +0000

    hanfing it over

--- wyattmain.cpp
+++ wyattmain.cpp
@@ -1,45 +1,6 @@
 #include "BST.cpp"
-int main(){
-  BST<int> *w = new BST<int>();
-  int option = 0;
-  // TreeNode *node = new TreeNode();
 
-  w->insert(5);
-  w->insert(10);
-  w->insert(3);
-  w->insert(12);
-  w->insert(8);
-  w->insert(4);
-  w->insert(2);
-  w->insert(13);
-
-  w-> printTree(w->getMax());
-  w-> printTree(w->getMin());
-
-  // w->deleteNode(2);
-
-  cout << w->search(2)<< endl;
-
-
-  // w->deleteNode(10);
-  // w->deleteNode(3);
-  // w->deleteNode(12);
-  // w->deleteNode(8);
-  // w->deleteNode(4);
-  // w->deleteNode(2);
-  // w->deleteNode(13);
-    w->deleteNode(5);
-
-  cout << "Test" << endl;
-
-  cout << "Is it in tree:" << w->search(5)<< endl;
-
-  cout << "Entire tree" << endl;
-
-  w-> printTree(w->getRoot());
-
-  // w->printTree(w->search(5));
-
+void printOptions(){
 
   cout << "\nWhat would you like to do?\n"<<endl;
   cout << "1) Print all students and their information (sorted by ascending id #)" << endl;
@@ -56,9 +17,18 @@ int main(){
   cout << "12) Remove an advisee from a faculty member given the ids " << endl;
   cout << "13) Rollback " << endl;
   cout << "14) Exit" << endl;
+}
+
+int main(){
+  BST<Person> *w = new BST<Person>();
+  int option = 0;
+
+  printOptions();
 
   cin >> option;
+  if (option == 1){
 
+  }
 
 
 
