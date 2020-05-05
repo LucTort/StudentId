186bf7348626d769497bd85a5623b4fff2e42ba6
wyattm14 <wmoe14@outlook.com>
2020-05-05 00:42:46 +0000

    fixing main

--- wyattmain.cpp
+++ wyattmain.cpp
@@ -1,6 +1,3 @@
-//Add in to a doubly linked list all the ids, then grab the ids in order, see if they match a get id from a student, and if it does, print all the student's information
-//If we make a function which prints all the student ids to a doubley linked list similar to how the printTree function prints all the student ids to the console, then we will be able to search every student id using that list
-
 #include "BST.cpp"
 
 void printOptions(){
@@ -23,61 +20,8 @@ void printOptions(){
 }
 
 int main(){
-
-//    ______          __     _____ __            __   ______             
-//   /_  __/__  _____/ /_   / ___// /___  ______/ /  /_  __/_______  ___ 
-//    / / / _ \/ ___/ __/   \__ \/ __/ / / / __  /    / / / ___/ _ \/ _ \
-//   / / /  __(__  ) /_    ___/ / /_/ /_/ / /_/ /    / / / /  /  __/  __/
-//  /_/  \___/____/\__/   /____/\__/\__,_/\__,_/    /_/ /_/   \___/\___/ 
-//                                                                       
-
-
-  BST<Person*> *w = new BST<Person*>();
-
-  Person *bob = new Person();
-  bob->setId(80);
-  bob->setGPA(3.29);
-
-  Person *frank = new Person();
-  frank->setId(30);
-  frank->setGPA(1.2);
-
-  Person *rosie = new Person();
-  rosie->setId(35);
-  rosie->setGPA(16);
-
-
-
-  w->insert(bob->getId(), bob);
-
-  w->insert(frank->getId(), frank);
-  w->insert(rosie->getId(), rosie);
-
-  w->printTree(w->getRoot());
-  cout<< "Delete" << endl;
-//      _______             __     __     __  _                      __                      __ 
-//     / ____(_)  __   ____/ /__  / /__  / /_(_)___  ____ _   ____ _/ /_   _________  ____  / /_
-//    / /_  / / |/_/  / __  / _ \/ / _ \/ __/ / __ \/ __ `/  / __ `/ __/  / ___/ __ \/ __ \/ __/
-//   / __/ / />  <   / /_/ /  __/ /  __/ /_/ / / / / /_/ /  / /_/ / /_   / /  / /_/ / /_/ / /_  
-//  /_/   /_/_/|_|   \__,_/\___/_/\___/\__/_/_/ /_/\__, /   \__,_/\__/  /_/   \____/\____/\__/  
-//                                                /____/                                        
-  w->deleteNode(bob->getId());
-    cout<< "Reprint tree" << endl;
-
-  w->printTree(w->getRoot());
-
-  w->returnNode(w->getRoot());
-
-
-
+  BST<Person> *w = new BST<Person>();
   int option = 0;
-
-//    ______          __     _____ __            __   ______                                 __
-//   /_  __/__  _____/ /_   / ___// /___  ______/ /  /_  __/_______  ___     ___  ____  ____/ /
-//    / / / _ \/ ___/ __/   \__ \/ __/ / / / __  /    / / / ___/ _ \/ _ \   / _ \/ __ \/ __  / 
-//   / / /  __(__  ) /_    ___/ / /_/ /_/ / /_/ /    / / / /  /  __/  __/  /  __/ / / / /_/ /  
-//  /_/  \___/____/\__/   /____/\__/\__,_/\__,_/    /_/ /_/   \___/\___/   \___/_/ /_/\__,_/   
-//                                                                                             
 
   printOptions();
 
